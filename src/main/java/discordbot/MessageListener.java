743ef693675bda4414a871cefb871de1c40b53af
jyomu <jyomu.developer@gmail.com>
2019-10-11 17:51:17 +0000

    first commit

---
+++ src/main/java/discordbot/MessageListener.java
@@ -0,0 +1,22 @@
+package discordbot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MessageListener extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDA jda = new JDABuilder(args[0]).build();
+        jda.addEventListener(new MessageListener());
+    }
+
+    @Override
+    public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
+        event.getJDA().getTextChannelsByName("通知", false).get(0).sendMessageFormat("%sさんが[🔊%s]に入りました",
+                event.getMember().getEffectiveName(), event.getChannelJoined().getName()).queue();
+    }
+
+}
