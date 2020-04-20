fdd43a9e27fc1d294476953fd6c86627223ba206
jyomu <jyomu.developer@gmail.com>
2020-04-20 09:19:52 +0000

    add Notification at left

--- src/main/java/discordbot/MessageListener.java
+++ src/main/java/discordbot/MessageListener.java
@@ -4,23 +4,32 @@ import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MessageListener extends ListenerAdapter {
-    public static void main(String[] args) throws LoginException {
-        JDA jda = new JDABuilder(args[0]).build();
+    public static void main(final String[] args) throws LoginException {
+        final JDA jda = new JDABuilder(args[0]).build();
         jda.addEventListener(new MessageListener());
     }
 
     @Override
-    public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
-        
-        if (event.getMember().getUser().isBot()) {
+    public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
+        if (event.getEntity().getUser().isBot()) {
             return;
         }
-        event.getGuild().getTextChannelsByName("通知", false).get(0).sendMessageFormat("%sさんが[🔊%s]に入りました",
-                event.getMember().getEffectiveName(), event.getChannelJoined().getName()).queue();
+        if (event.getChannelLeft() != null) {
+            event.getEntity().getGuild().getTextChannelsByName("通知", false).get(0)
+                    .sendMessageFormat("%sさんが[🔊%s]から出ました", event.getEntity().getEffectiveName(),
+                            event.getChannelLeft().getName())
+                    .queue();
+        }
+        if (event.getChannelJoined() != null) {
+            event.getEntity().getGuild().getTextChannelsByName("通知", false).get(0)
+                    .sendMessageFormat("%sさんが[🔊%s]に入りました", event.getEntity().getEffectiveName(),
+                            event.getChannelJoined().getName())
+                    .queue();
+        }
     }
 
 }
